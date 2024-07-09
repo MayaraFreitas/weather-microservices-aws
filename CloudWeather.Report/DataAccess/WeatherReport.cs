6553feb100ee3d8165254f5dc32bb5ddfae36d48
Mayara <mah.alvesfreitas@gmail.com>
2024-07-09 17:24:21 -0300

    add data acces for temperature and report microservices

---
+++ CloudWeather.Report/DataAccess/WeatherReport.cs
@@ -0,0 +1,13 @@
+﻿namespace CloudWeather.Report.DataAccess
+{
+    public class WeatherReport
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public decimal AvarageHighF { get; set; }
+        public decimal AvarageLowF { get; set; }
+        public decimal RainfallTotalInches { get; set; }
+        public decimal SnowTotalInches { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
